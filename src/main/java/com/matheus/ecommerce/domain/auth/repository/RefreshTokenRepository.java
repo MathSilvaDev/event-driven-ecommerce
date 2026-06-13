c3c10d9e280ec03d30ccda924f5b126281a2553e
Matheus Silva <mathsilva.code@gmail.com>
2026-06-13 11:18:00 -0300

    feat: add auth repositories

---
+++ src/main/java/com/matheus/ecommerce/domain/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,15 @@
+package com.matheus.ecommerce.domain.auth.repository;
+
+import com.matheus.ecommerce.domain.auth.entity.RefreshToken;
+import com.matheus.ecommerce.domain.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+
+    Optional<RefreshToken> findByUser(User user);
+}
